e5169c6e4f8ff43bb38f19344508b9f3ce793489
Mateo <mortiz-2024204@kinal.edu.gt>
2026-07-01 11:04:39 -0600

    Feat: Se agrego la data de medicos

---
+++ ProyectoFinal-ts/src/data/medicos.ts
@@ -0,0 +1,12 @@
+import { Medico } from "../models/medico";
+
+export const medicos: Medico[] = [
+
+    new Medico(1,"Carlos","Mendez",1,"55520001","MED-001"),
+    new Medico(2,"Laura","Castillo",2,"55520002","MED-002"),
+    new Medico(3,"Roberto","Sanchez",3,"55520003","MED-003"),
+    new Medico(4,"Diana","Morales",4,"55520004","MED-004"),
+    new Medico(5,"Fernando","Reyes",5,"55520005","MED-005"),
+    new Medico(6,"Gabriela","Flores",6,"55520006","MED-006")
+
+];
