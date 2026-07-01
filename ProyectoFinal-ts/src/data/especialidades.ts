ba52a469ad80c12117223438dd4b6ccedc4d0a26
Mateo <mortiz-2024204@kinal.edu.gt>
2026-07-01 11:08:44 -0600

    Feat: Se agrego la data de especialidades

---
+++ ProyectoFinal-ts/src/data/especialidades.ts
@@ -0,0 +1,12 @@
+import { Especialidad } from "../models/especialidad";
+
+export const especialidades: Especialidad[] = [
+
+    new Especialidad(1,"Cardiologia","Enfermedades del corazon y sistema circulatorio"),
+    new Especialidad(2,"Dermatologia","Enfermedades de la piel"),
+    new Especialidad(3,"Pediatria","Enfermedades en niños"),
+    new Especialidad(4,"Ortopedia","Problemas del sistema musculoesqueletico"),
+    new Especialidad(5,"Neurologia","Enfermedades del sistema nervioso"),
+    new Especialidad(6,"Medicina General","Atencion medica primaria")
+
+];
