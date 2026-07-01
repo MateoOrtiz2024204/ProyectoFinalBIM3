0de96754f97cc6ff78ddcb70a2c550bf5a72ccf5
Mateo <mortiz-2024204@kinal.edu.gt>
2026-07-01 11:03:07 -0600

    Feat: Se agrego la data de administradores

---
+++ ProyectoFinal-ts/src/data/administradores.ts
@@ -0,0 +1,12 @@
+import { Administrador } from "../models/administrador";
+
+export const administradores: Administrador[] = [
+
+    new Administrador(
+        1,
+        "Administrador",
+        "Principal",
+        "Administración"
+    )
+
+];
