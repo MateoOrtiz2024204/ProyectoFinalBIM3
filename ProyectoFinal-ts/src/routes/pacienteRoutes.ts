2c8cc185290b8b45e9bdfb0373c30481cb7ede2d
Mateo <mortiz-2024204@kinal.edu.gt>
2026-07-27 19:38:23 -0600

    Feat: Se agrego pacienteRoutes a la carpeta routes

---
+++ ProyectoFinal-ts/src/routes/pacienteRoutes.ts
@@ -0,0 +1,19 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { PacienteController } from "../controllers/pacienteController";
+import { enviarError } from "../utils/http";
+
+const controller = new PacienteController();
+
+export async function pacienteRoutes(req: IncomingMessage, res: ServerResponse, partes: string[]) {
+    const id = partes[0] ? Number(partes[0]) : undefined;
+    try {
+        if (req.method === "GET" && id === undefined) return controller.listar(req, res);
+        if (req.method === "GET" && id !== undefined) return controller.buscarPorId(req, res, id);
+        if (req.method === "POST") return controller.agregar(req, res);
+        if (req.method === "PUT" && id !== undefined) return controller.actualizar(req, res, id);
+        if (req.method === "DELETE" && id !== undefined) return controller.eliminar(req, res, id);
+        enviarError(res, 405, "Metodo no permitido");
+    } catch {
+        enviarError(res, 500, "Error interno del servidor");
+    }
+}
