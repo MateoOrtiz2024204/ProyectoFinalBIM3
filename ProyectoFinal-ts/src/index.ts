698b84c535b21594e2c0edd1c2b28bd6cd70235c
Mateo <mortiz-2024204@kinal.edu.gt>
2026-07-27 19:43:43 -0600

    Feat: Se modifico el index para que prenda el server

--- ProyectoFinal-ts/src/index.ts
+++ ProyectoFinal-ts/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+import "dotenv/config";
+import { server } from "./server";
+
+const PORT = Number(process.env.PORT) || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
